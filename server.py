e9e2aca5eb8f54d8a07a53c5c3b33ebbe1203e1e
Fernando Monje <fcmonje@gmail.com>
2018-08-08 11:08:38 -0300

    Added logging and exception handling to server.py, and more internal features. Added method in base class to get 'X-Forwarded-For' header from Proxy Requests

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ import datetime
 import re
 import StringIO
 import cx_Oracle
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 # Global Variables Denifitions
 global DB_USER
@@ -22,26 +26,54 @@ DB_USER='scada'
 DB_PASS='sca#0406'
 DB_HOST='smcx5-scan'
 DB_SCHEMA='compp'
-DB_CONNECTION = cx_Oracle.connect(DB_USER + '/' + DB_PASS + '@' + DB_HOST + '/' + DB_SCHEMA)
-
 HOST_NAME = '10.100.102.52'
 PORT_NUMBER = 9995
 API_VERSION = 'v1'
 BASE_URL = '/api/v1'
+LOG_FILE_NAME = 'ptb_performance_server.log'
+LOG_MAX_SIZE_MB = 50 * 1024 * 1024
+LOG_LEVEL = 'DEBUG'
+
+serverLogger = logging.getLogger('server-Logger')
+defaultLogHandler = RotatingFileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), LOG_FILE_NAME), maxBytes=LOG_MAX_SIZE_MB, backupCount=10)
+serverLogger.setLevel(eval('logging.' + LOG_LEVEL))
+frmt = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
+defaultLogHandler.setFormatter(frmt)
+serverLogger.addHandler(defaultLogHandler)
+
+try:
+  DB_CONNECTION = cx_Oracle.connect(DB_USER + '/' + DB_PASS + '@' + DB_HOST + '/' + DB_SCHEMA)
+except Exception, e:
+  serverLogger.error('No connection could be made to database.')
+  serverLogger.error('Exception: ' + e)
+  serverLogger.error('Server will not start.')
+  sys.exit(1)
 
 class RequestHandler(BaseHTTPRequestHandler):
     server_version = 'Portability Performance Server/1.0'
     sys_version = ''
+    def log_message(self, format, *args):
+        serverLogger.info("%s - - %s" % (self.real_address_string(),format%args))
+
+    def real_address_string(self):
+      if 'X-Forwarded-For' in self.headers:
+        return self.headers['X-Forwarded-For']
+      else:
+        return self.address_string()
 
     def _check_spid_status(self, spid):
       query = 'SELECT COUNT(1) FROM PTB_MEASURE_SPID WHERE spid = :spid AND STATUS = 1'
-      cur = DB_CONNECTION.cursor()
-      cur.execute(query, {'spid':  spid})
-      status = cur.fetchone()
-      cur.close()
-      if status[0] == 1:
-        return True
-      else:
+      try:
+        cur = DB_CONNECTION.cursor()
+        cur.execute(query, {'spid':  spid})
+        status = cur.fetchone()
+        cur.close()
+        if status[0] == 1:
+          return True
+        else:
+          return False
+      except Exception, e:
+        serverLogger.error('Exception ocurred in database operation: ' + e)
         return False
 
     def _setCORSHeaders(self, res, method, content="application/json"):
@@ -51,25 +83,23 @@ class RequestHandler(BaseHTTPRequestHandler):
       res.send_header("Content-type", content)
       res.end_headers()
 
-    def do_HEAD(s):
-      s.send_response(200)
-      s._setCORSHeaders(s, "PUT,POST,GET")
-      s.send_header("Content-type", "application/json")
-      s.end_headers()
-      return
     def do_GET(s):
       """Response for a GET request."""
       if s.path == BASE_URL + '/data':
-        outfile = open('./dummy.file', 'r')
-        #time.sleep(5)
-        outfile.seek(0,2)
-        file_size = int(outfile.tell())
-        s.send_response(200)
-        s.send_header("Content-Length", file_size)
-        s._setCORSHeaders(s, "GET", "application/octet-stream")
-        outfile.seek(0,0)
-        s.wfile.write(outfile.read())
-        outfile.close()
+        try:
+          outfile = open('./dummy.file', 'r')
+          outfile.seek(0,2)
+          file_size = int(outfile.tell())
+          s.send_response(200)
+          s.send_header("Content-Length", file_size)
+          s._setCORSHeaders(s, "GET", "application/octet-stream")
+          outfile.seek(0,0)
+          s.wfile.write(outfile.read())
+          outfile.close()
+        except Exception, e:
+          serverLogger.error('Exception in GET method: ' + str(e))
+          s.send_response(500, 'Internal Server Error')
+          s.end_headers()
       else:
         s.send_response(400, 'Invalid Method. Try GET instead')
         s.end_headers()
@@ -78,51 +108,69 @@ class RequestHandler(BaseHTTPRequestHandler):
       """ Response for a POST request."""
       spid_regex = re.compile(BASE_URL + '/carrier/[0-9]{4}/measurement')
       if s.path == BASE_URL + '/data/upload':
-        start_request_time = datetime.datetime.now()
-        content_length = int(s.headers['Content-Length'])
-        file_content = StringIO.StringIO()
-        file_content.write(s.rfile.read(content_length))
-        file_content.seek(0,2)
-        file_content.close()
-        s.send_response(200)
-        s._setCORSHeaders(s, "POST", "text/html")
-        end_request_time = datetime.datetime.now()
+        try:
+          start_request_time = datetime.datetime.now()
+          content_length = int(s.headers['Content-Length'])
+          file_content = StringIO.StringIO()
+          file_content.write(s.rfile.read(content_length))
+          file_content.seek(0,2)
+          file_content.close()
+          s.send_response(200)
+          s._setCORSHeaders(s, "POST", "text/html")
+          end_request_time = datetime.datetime.now()
+        except Exception, e:
+          serverLogger.error('Exception in POST method: ' + str(e))
+          s.send_response(500, 'Internal Server Error')
+          s.end_headers()
       elif spid_regex.search(s.path):
         url_spid = s.path.split('/')[4]
+        json_loaded = []
         content_length = int(s.headers['Content-Length'])
         file_content = s.rfile.read(content_length)
-        json_loaded = simplejson.loads(file_content)
+        try:
+          json_loaded = simplejson.loads(file_content)
+        except Exception, e:
+          serverLogger.error('Cold not read json: ' + str(e))
+          s.send_response(500, 'Internal Server Error')
+          s.end_headers()
         if json_loaded['spid'] != url_spid:
           s.send_response(401, 'Invalid SPID from Request o End Point.')
           s.end_headers()
         else:
           if s._check_spid_status(url_spid):
-            cur = DB_CONNECTION.cursor()
-            params_list = [ 'spid',
-                            'upload_bandwidth',
-                            'download_bandwidth',
-                            'ping_response_time',
-                            'ping_packet_loss',]
-            for param in params_list:
-              if not param in json_loaded:
-                s.send_response(401, 'Json Missing Parameters.')
-                s.end_headers()
-                return
-            cur.prepare('INSERT INTO SCADA.PTB_MEASURE_DATA (SPID, DATETIME, UPLOAD_BANDWIDTH, DOWNLOAD_BANDWIDTH, PING_RESPONSE_TIME, PING_PACKET_LOSS) VALUES (:SPID, SYSDATE, :UPLOAD_BANDWIDTH, :DOWNLOAD_BANDWIDTH, :PING_RESPONSE_TIME, :PING_PACKET_LOSS)')
-            cur.execute(None, {'SPID': json_loaded['spid'],
-                               'UPLOAD_BANDWIDTH': str(json_loaded['upload_bandwidth']),
-                               'DOWNLOAD_BANDWIDTH' : str(json_loaded['download_bandwidth']),
-                               'PING_RESPONSE_TIME' : str(json_loaded['ping_response_time']),
-                              'PING_PACKET_LOSS' : str(json_loaded['ping_packet_loss'])})
-            DB_CONNECTION.commit()
-            cur.close()
-            #print "SPID Recebido: %s" % (json_loaded['spid'])
-            #print "Upload Bandwidth Recebido: %s" % (json_loaded['upload_bandwidth'])
-            #print "Download Bandwidth Recebido: %s" % (json_loaded['download_bandwidth'])
-            #print "Ping Response Time Recebido: %s" % (json_loaded['ping_response_time'])
-            #print "Ping Packet Lost Avg: %s" % (json_loaded['ping_packet_loss'])
-            s.send_response(200)
-            s._setCORSHeaders(s, "POST", "text/html")
+            try:
+              cur = DB_CONNECTION.cursor()
+              params_list = [ 'spid',
+                              'upload_bandwidth',
+                              'download_bandwidth',
+                              'ping_response_time',
+                              'ping_packet_loss',]
+              for param in params_list:
+                if not param in json_loaded:
+                  s.send_response(401, 'Json Missing Parameters.')
+                  s.end_headers()
+                  return
+              cur.prepare('INSERT INTO SCADA.PTB_MEASURE_DATA (SPID, DATETIME, UPLOAD_BANDWIDTH, DOWNLOAD_BANDWIDTH, \
+                           PING_RESPONSE_TIME, PING_PACKET_LOSS) VALUES (:SPID, SYSDATE, :UPLOAD_BANDWIDTH, :DOWNLOAD_BANDWIDTH, \
+                           :PING_RESPONSE_TIME, :PING_PACKET_LOSS)')
+              cur.execute(None, {'SPID': json_loaded['spid'],
+                                 'UPLOAD_BANDWIDTH': str(json_loaded['upload_bandwidth']),
+                                 'DOWNLOAD_BANDWIDTH' : str(json_loaded['download_bandwidth']),
+                                 'PING_RESPONSE_TIME' : str(json_loaded['ping_response_time']),
+                                 'PING_PACKET_LOSS' : str(json_loaded['ping_packet_loss'])})
+            except Exception, e:
+              serverLogger.error('Failed to get/save data from json to database: ' + str(e))
+              s.send_response(500, 'Internal Server Error')
+              s.end_headers()
+            try:
+              DB_CONNECTION.commit()
+              cur.close()
+              s.send_response(200)
+              s._setCORSHeaders(s, "POST", "text/html")
+            except Exception, e:
+              serverLogger.error('Failed to commit data to database: ' + str(e))
+              s.send_response(500, 'Internal Server Error')
+              s.end_headers()
           else:
             s.send_response(401, 'SPID Inactive at Server.')
             s.end_headers()
@@ -137,11 +185,11 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 if __name__ == '__main__':
     server_class = ThreadedHTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), RequestHandler)
-    print time.asctime(), "Starting Portability Performance Server - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    serverLogger.warn('Starting Portability Performance Server - %s:%s' % (HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
     DB_CONNECTION.close()
-    print time.asctime(), "Portability Performance Server stopped - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    serverLogger.warn('Portability Performance Server stopped - %s:%s' % (HOST_NAME, PORT_NUMBER))
