39d888e78ba1325d5a5403986c8d64872a30927d
Fernando Monje <fcmonje@gmail.com>
2018-08-08 08:49:53 -0300

    Major adjustments in server.py - Added Multi-threading and Database Connection Using cx_Oracle

--- server.py
+++ server.py
@@ -3,16 +3,28 @@ import time
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import threading
-import json
+import simplejson
 import time
 import datetime
 import re
 import StringIO
+import cx_Oracle
 
 # Global Variables Denifitions
-# Global Variables Settings
+global DB_USER
+global DB_PASS
+global DB_HOST
+global DB_SCHEMA
+global DB_CONNECTION
 
-HOST_NAME = '10.200.120.8'
+# Global Variables Settings
+DB_USER='scada'
+DB_PASS='sca#0406'
+DB_HOST='smcx5-scan'
+DB_SCHEMA='compp'
+DB_CONNECTION = cx_Oracle.connect(DB_USER + '/' + DB_PASS + '@' + DB_HOST + '/' + DB_SCHEMA)
+
+HOST_NAME = '10.100.102.52'
 PORT_NUMBER = 9995
 API_VERSION = 'v1'
 BASE_URL = '/api/v1'
@@ -20,6 +32,17 @@ BASE_URL = '/api/v1'
 class RequestHandler(BaseHTTPRequestHandler):
     server_version = 'Portability Performance Server/1.0'
     sys_version = ''
+
+    def _check_spid_status(self, spid):
+      query = 'SELECT COUNT(1) FROM PTB_MEASURE_SPID WHERE spid = :spid AND STATUS = 1'
+      cur = DB_CONNECTION.cursor()
+      cur.execute(query, {'spid':  spid})
+      status = cur.fetchone()
+      cur.close()
+      if status[0] == 1:
+        return True
+      else:
+        return False
 
     def _setCORSHeaders(self, res, method, content="application/json"):
       res.send_header("Access-Control-Allow-Origin", "*");
@@ -68,18 +91,41 @@ class RequestHandler(BaseHTTPRequestHandler):
         url_spid = s.path.split('/')[4]
         content_length = int(s.headers['Content-Length'])
         file_content = s.rfile.read(content_length)
-        json_loaded = json.loads(file_content)
+        json_loaded = simplejson.loads(file_content)
         if json_loaded['spid'] != url_spid:
-          s.send_response(401, 'Invalid SPID.')
+          s.send_response(401, 'Invalid SPID from Request o End Point.')
           s.end_headers()
         else:
-          print "SPID Recebido: %s" % (json_loaded['spid'])
-          print "Upload Bandwidth Recebido: %s" % (json_loaded['upload_bandwidth'])
-          print "Download Bandwidth Recebido: %s" % (json_loaded['download_bandwidth'])
-          print "Ping Response Time Recebido: %s" % (json_loaded['ping_response_time'])
-          print "Ping Packet Lost Avg: %s" % (json_loaded['ping_packet_loss'])
-          s.send_response(200)
-          s._setCORSHeaders(s, "POST", "text/html")
+          if s._check_spid_status(url_spid):
+            cur = DB_CONNECTION.cursor()
+            params_list = [ 'spid',
+                            'upload_bandwidth',
+                            'download_bandwidth',
+                            'ping_response_time',
+                            'ping_packet_loss',]
+            for param in params_list:
+              if not param in json_loaded:
+                s.send_response(401, 'Json Missing Parameters.')
+                s.end_headers()
+                return
+            cur.prepare('INSERT INTO SCADA.PTB_MEASURE_DATA (SPID, DATETIME, UPLOAD_BANDWIDTH, DOWNLOAD_BANDWIDTH, PING_RESPONSE_TIME, PING_PACKET_LOSS) VALUES (:SPID, SYSDATE, :UPLOAD_BANDWIDTH, :DOWNLOAD_BANDWIDTH, :PING_RESPONSE_TIME, :PING_PACKET_LOSS)')
+            cur.execute(None, {'SPID': json_loaded['spid'],
+                               'UPLOAD_BANDWIDTH': str(json_loaded['upload_bandwidth']),
+                               'DOWNLOAD_BANDWIDTH' : str(json_loaded['download_bandwidth']),
+                               'PING_RESPONSE_TIME' : str(json_loaded['ping_response_time']),
+                              'PING_PACKET_LOSS' : str(json_loaded['ping_packet_loss'])})
+            DB_CONNECTION.commit()
+            cur.close()
+            #print "SPID Recebido: %s" % (json_loaded['spid'])
+            #print "Upload Bandwidth Recebido: %s" % (json_loaded['upload_bandwidth'])
+            #print "Download Bandwidth Recebido: %s" % (json_loaded['download_bandwidth'])
+            #print "Ping Response Time Recebido: %s" % (json_loaded['ping_response_time'])
+            #print "Ping Packet Lost Avg: %s" % (json_loaded['ping_packet_loss'])
+            s.send_response(200)
+            s._setCORSHeaders(s, "POST", "text/html")
+          else:
+            s.send_response(401, 'SPID Inactive at Server.')
+            s.end_headers()
       else:
         s.send_response(400, 'Invalid Path.')
         s.end_headers()
@@ -97,4 +143,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+    DB_CONNECTION.close()
     print time.asctime(), "Portability Performance Server stopped - %s:%s" % (HOST_NAME, PORT_NUMBER)
